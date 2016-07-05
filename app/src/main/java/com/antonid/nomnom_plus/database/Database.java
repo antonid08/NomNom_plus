628e0732a6cf98de6fcfb2f34534c335f8ad53ef
antonid <antonenkoilya01@gmail.com>
2016-07-05 12:53:53 +0300

    Check previous comit :)

---
+++ app/src/main/java/com/antonid/nomnom_plus/database/Database.java
@@ -0,0 +1,113 @@
+package com.antonid.nomnom_plus.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
+
+/**
+ * Created by antonid on 12.07.2015.
+ */
+
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database {
+    private SQLiteOpenHelper openHelper;
+    private SQLiteDatabase database;
+    private static Database instance;
+
+
+    public static abstract class IngredientsTable implements BaseColumns{
+        private static final String TABLE_NAME = "Ingredients";
+        private static final String ID = "_id";
+        private static final String NAME = "name";
+        private static final String IS_CHECKED = "checked";
+        private static final String FOR_WHAT_RECIPES = "forWhatRecipes";
+
+    }
+
+    public static abstract class RecipesTable implements BaseColumns{
+        private static final String TABLE_NAME = "Recipes";
+        private static final String NAME = "Name";
+        private static final String ID = "_id";
+        private static final String INGREDIENTS = "ingredients";
+        private static final String HOW_TO_COOK = "howToCook";
+        private static final String IS_AVAILABLE = "isAvailable";
+        private static final String NUMBER_OF_STEPS = "numberOfSteps";
+        private static final String TIME_FOR_COOKING = "timeForCooking";
+        private static final String DESCRIPTION = "description";
+        private static final String NUMBER_OF_PERSONS = "numberOfPersons";
+        private static final String NUMBER_OF_EVERY_ING = "numberOfEveryIng";
+        private static final String NUMBER_OF_INGREDIENTS = "numberOfIngredients";
+        private static final String MEASURE_FOR_TIME = "measureForTime";
+    }
+
+
+    public static abstract class RecipesCategoriesTable implements BaseColumns{
+        private static final String TABLE_NAME = "CategoriesOfRecipes";
+        private static final String ID = "_id";
+        private static final String NAME = "name";
+        private static final String RECIPES = "recipes";
+        private static final String NUMBER_OF_RECIPES = "numberOfRecipes";
+        private static final String DESCRIPTION = "description";
+
+    }
+
+    public static abstract class CategoriesOfIngredientsTable implements BaseColumns{
+        public static final String TABLE_NAME = "Categories";
+        private static final String ID = "_id";
+        private static final String NAME= "name";
+        private static final String INGREDIENTS = "ingredients";
+        private static final String NUMBER_OF_INGREDIENTS = "numberOfIngredients";
+        private static final String EXAMPLE = "example";
+    }
+
+    /**
+     * Private constructor to avoid object creation from outside classes.
+     *
+     * @param context
+     */
+    private Database(Context context) {
+        this.openHelper = new DatabaseOpenHelper(context);
+    }
+
+    /**
+     * Return a singleton instance of DatabaseAccess.
+     *
+     * @param context the Context
+     * @return the instance of DabaseAccess
+     */
+    public static Database getInstance(Context context) {
+        if (instance == null) {
+            instance = new Database(context);
+        }
+        return instance;
+    }
+
+    /**
+     * Open the database connection.
+     */
+    public void open() {
+        this.database = openHelper.getWritableDatabase();
+    }
+
+    /**
+     * Close the database connection.
+     */
+    public void close() {
+        if (database != null) {
+            this.database.close();
+        }
+    }
+
+    /**
+     * Read all quotes from the database.
+     *
+     * @return a List of quotes
+     */
+}
+
